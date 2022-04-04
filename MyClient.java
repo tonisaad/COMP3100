95ad42429f0365937c54bbd13fe2e26d67e7e661
Saad Farooq <saad.faroq@students.mq.edu.au>
2022-04-04 22:48:09 +1000

    Refactored the code of MyClient.java added some indentions

--- MyClient.java
+++ MyClient.java
@@ -1,150 +1,139 @@
-    import java.io.*;
+import java.io.*;
     import java.net.*;
     import java.util.ArrayList;
     public class MyClient {
 
-      private int serverNumber;
-      private static String serverType="yes";
-      private static int serverID=0;
-      private static int serverCore;
-      private static int jobID;
-      private static int largestServerCount;
-      private static int remainder;
-      private static ArrayList < String > servers = new ArrayList < String > ();
-      private static boolean flag = false;
+        private int serverNumber;
+        private static String serverType = "yes";
+        private static int serverID = 0;
+        private static int serverCore;
+        private static int jobID;
+        private static int largestServerCount;
+        private static int remainder;
+        private static ArrayList < String > servers = new ArrayList < String > ();
+        private static boolean flag = false;
 
-      public static void main(String[] args) {
-        try {
-          Socket s = new Socket("127.0.0.1", 50000);
-          DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-          BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        public static void main(String[] args) {
+            try {
+                Socket s = new Socket("127.0.0.1", 50000);
+                DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+                BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
 
-          System.out.println("Target IP: " + s.getInetAddress() + " Target Port: " + s.getPort());
+                System.out.println("Target IP: " + s.getInetAddress() + " Target Port: " + s.getPort());
 
-          dout.write(("HELO\n").getBytes());
-          dout.flush();
-          String str = in .readLine();
-          System.out.println(str);
+                dout.write(("HELO\n").getBytes());
+                dout.flush();
+                String str = in .readLine();
+                System.out.println(str);
 
-          String username = System.getProperty("user.name");
-          System.out.println("SENT: AUTH");
-          dout.write(("AUTH " + username + "\n").getBytes());
-          str = in .readLine();
-          System.out.println("RCVD: " + str);
+                String username = System.getProperty("user.name");
+                System.out.println("SENT: AUTH");
+                dout.write(("AUTH " + username + "\n").getBytes());
+                str = in .readLine();
+                System.out.println("RCVD: " + str);
 
-          dout.write(("REDY\n").getBytes());
-          dout.flush();
-          str = in .readLine();
+                dout.write(("REDY\n").getBytes());
+                dout.flush();
+                str = in .readLine();
 
-          while (!str.equals("NONE")) {
-            System.out.println(str);
+                while (!str.equals("NONE")) {
+                    System.out.println(str);
 
-            System.out.println("RCVD1: " + str);
-            String[] jobInfo = str.split(" ");
-            jobID = Integer.parseInt(jobInfo[2]);
+                    System.out.println("RCVD1: " + str);
+                    String[] jobInfo = str.split(" ");
+                    jobID = Integer.parseInt(jobInfo[2]);
 
-            if (jobInfo[0].equals("JCPL")) {
-              dout.write(("REDY\n").getBytes());
-              dout.flush();
-              str = in .readLine();
-            } else if (jobInfo[0].equals("JOBN")) {
+                    if (jobInfo[0].equals("JCPL")) {
+                        dout.write(("REDY\n").getBytes());
+                        dout.flush();
+                        str = in .readLine();
+                    } else if (jobInfo[0].equals("JOBN")) {
 
-              dout.write(("GETS Capable: " + jobInfo[4] + " " + jobInfo[5] + " " + jobInfo[6] + "\n").getBytes());
-              dout.flush();
-              str = in .readLine();
-              System.out.println("RCVD2: " + str);
+                        dout.write(("GETS Capable: " + jobInfo[4] + " " + jobInfo[5] + " " + jobInfo[6] + "\n").getBytes());
+                        dout.flush();
+                        str = in .readLine();
+                        System.out.println("RCVD2: " + str);
 
-              String[] Info = str.split(" ");
-              int serverCount = Integer.parseInt(Info[1]);
+                        String[] Info = str.split(" ");
+                        int serverCount = Integer.parseInt(Info[1]);
 
-              dout.write(("OK\n").getBytes());
-              dout.flush();
-              
-                   if (flag!=false){
-                   for (int i=0;i<serverCount;i++){
-                   str=in.readLine();
-                   }
-                   }if(flag!=true){
-              
-               
-                  for (int i = 0; i < serverCount; i++) {
-                  
-                  str = in.readLine();
-                  servers.add(str);
-              
-                  String serverInfo[] = str.split(" ");
-                    
-                  if (Integer.parseInt(serverInfo[4]) > serverCore) {
-                    serverType = serverInfo[0];
-                    serverID = Integer.parseInt(serverInfo[1]);
-                    serverCore = Integer.parseInt(serverInfo[4]);
-                    
-                    // System.out.println(str);
-                    // servers.add(serverID);
-                   
-                  } 
-                  
-                   //if (largestserverType.equals(serverInfo[0]) && serverCore == Integer.parseInt(serverInfo[4])) {
-                   // largestServerCount++;
-                    //flag = true;
-                    //servers.add(serverID);
-                  //}
-                  
-                }
-                
-              }
-              
-              System.out.println("largest server type: " + serverType);
-              System.out.println("maxCore: " + serverCore);
-              System.out.println("istSize: " + servers.size());
-              
-              if(flag!=true){
-              for (int i=0; i<servers.size();i++){
-              String string [] = servers.get(i).split(" ");
-               if (serverType.equals(string[0]) && serverCore == Integer.parseInt(string[4])) {
-                    largestServerCount++;
-                    flag=true;
+                        dout.write(("OK\n").getBytes());
+                        dout.flush();
+
+                        if (flag != false) {
+                            for (int i = 0; i < serverCount; i++) {
+                                str = in .readLine();
+                            }
+                        }
+                        if (flag != true) {
+
+
+                            for (int i = 0; i < serverCount; i++) {
+
+                                str = in .readLine();
+                                servers.add(str);
+
+                                String serverInfo[] = str.split(" ");
+
+                                if (Integer.parseInt(serverInfo[4]) > serverCore) {
+                                    serverType = serverInfo[0];
+                                    serverID = Integer.parseInt(serverInfo[1]);
+                                    serverCore = Integer.parseInt(serverInfo[4]);
+                                }
+                            }
+                        }
+
+                        System.out.println("largest server type: " + serverType);
+                        System.out.println("maxCore: " + serverCore);
+                        System.out.println("istSize: " + servers.size());
+
+                        if (flag != true) {
+                            for (int i = 0; i < servers.size(); i++) {
+                                String string[] = servers.get(i).split(" ");
+                                if (serverType.equals(string[0]) && serverCore == Integer.parseInt(string[4])) {
+                                    largestServerCount++;
+                                    flag = true;
+                                }
+                            }
+                        }
+
+
+                        System.out.println(largestServerCount);
+
+                        dout.write(("OK\n").getBytes());
+                        dout.flush();
+                        System.out.println(str);
+                        str = in .readLine();
+                        System.out.println(str);
+                        System.out.println("RCVD3: " + str);
+
+                        remainder = jobID % largestServerCount;
+                        serverID = remainder;
+
+
+                        dout.write(("SCHD " + jobID + " " + serverType + " " + serverID + "\n").getBytes());
+                        dout.flush();
+                        str = in .readLine();
+                        System.out.println("RCVD4: " + str);
+
+                        dout.write(("REDY\n").getBytes());
+                        dout.flush();
+                        str = in .readLine();
+                        System.out.println(str);
                     }
-                  }
-              }
-              
+                }
 
-              System.out.println(largestServerCount);
+                System.out.println("SENT: QUIT");
+                dout.write(("QUIT\n").getBytes());
+                str = in .readLine();
+                System.out.println("RCVD: " + str);
 
-              dout.write(("OK\n").getBytes());
-              dout.flush();
-              System.out.println(str);
-              str = in.readLine();
-              System.out.println(str);
-              System.out.println("RCVD3: " + str);
+                in .close();
+                dout.close();
+                s.close();
 
-              remainder = jobID % largestServerCount;
-              serverID = remainder;
-              
-
-              dout.write(("SCHD " + jobID + " " + serverType + " " + serverID + "\n").getBytes());
-              dout.flush();
-              str = in .readLine();
-              System.out.println("RCVD4: " + str);
-
-              dout.write(("REDY\n").getBytes());
-              dout.flush();
-              str = in .readLine();
-              System.out.println(str);
+            } catch (Exception e) {
+                System.out.println(e);
             }
-          }
-
-          System.out.println("SENT: QUIT");
-          dout.write(("QUIT\n").getBytes());
-          str = in .readLine();
-          System.out.println("RCVD: " + str);
-
-          in .close();
-          dout.close();
-          s.close();
-
-        } catch (Exception e) {
-          System.out.println(e);
         }
-      }
     }
